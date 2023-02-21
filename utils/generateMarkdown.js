da12f0352d78a118e16babe1a632d4d91482007d
Obee123 <aobiaderi@yahoo.com>
2023-02-21 10:47:49 +0000

    updated generate markdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,29 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title} 
+  
+  ## Description
+    ${data.description}
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributors](#contributors)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  ## Installation
+  Packages required to run this program are: ${data.installation}
+  
+  ## Usage
+  Examples of how to use this program: ${data.usage}
+  ## License
+  ${data.license}
+  ## Tests
+  To test, run the following command: ${data.tests}
+  ## Contributors
+  ${data.contributer}
+
 
 `;
 }
