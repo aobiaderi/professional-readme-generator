16bcd6e2bdc4e72a8463a389e185c41175be4724
Obee123 <aobiaderi@yahoo.com>
2023-02-21 10:29:16 +0000

    added array of questions to index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const path = require('path');
+// const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
@@ -8,6 +8,68 @@ const questions = [
 
 ];
 
+// Questions User Answers
+function askQuestions() {
+    return inquirer.prompt([
+      {
+        type: "input",
+        message: "What is your user GitHub username?",
+        name: "username"
+      },
+      {
+        type: "input",
+        message: "What is your email?",
+        name: "email"
+      },
+      {
+        type: "input",
+        message: "What is URL of your project in GitHub?",
+        name: "url"
+      },
+      {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title"
+      },
+      {
+        type: "input",
+        message: "Please provide a description of your project.",
+        name: "description"
+      },
+      {
+        type: "input",
+        message: "What packages need to be installed to run your project.",
+        name: "installation"
+      },
+      {
+        type: "input",
+        message: "What technologies were used to create your project.",
+        name: "technology"
+      },
+      {
+        type: "input",
+        message: "Please provide an example of how your project can be used.",
+        name: "usage"
+      },
+      {
+        type: "list",
+        name: "license",
+        message: "What kind of license would you like to have?",
+        name: "license",
+        choices: ["MIT", "APACHE 2.0", "GPL v3", "BSD 3", "None"]
+      },
+      {
+        type: "input",
+        message: "Including yourself, please list out all contributors",
+        name: "contributer"
+      },
+      {
+        type: "input",
+        message: "What command is used to run a test",
+        name: "tests",
+      }
+    ]); // end of return 
+  }; // end of function
 // function to write README file
 function writeToFile(fileName, data) {
 }
