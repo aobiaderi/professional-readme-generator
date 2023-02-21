e6a2e3f5689d8be54cb5248ad2041ac7f98f7a25
Obee123 <aobiaderi@yahoo.com>
2023-02-21 12:37:53 +0000

    updated readme generator and writetofile functions

--- index.js
+++ index.js
@@ -16,53 +16,53 @@ function askQuestions() {
         message: "What is your email?",
         name: "email"
       },
-      {
-        type: "input",
-        message: "What is URL of your project in GitHub?",
-        name: "url"
-      },
-      {
-        type: "input",
-        message: "What is the title of your project?",
-        name: "title"
-      },
-      {
-        type: "input",
-        message: "Please provide a description of your project.",
-        name: "description"
-      },
-      {
-        type: "input",
-        message: "What packages need to be installed to run your project.",
-        name: "installation"
-      },
-      {
-        type: "input",
-        message: "What technologies were used to create your project.",
-        name: "technology"
-      },
-      {
-        type: "input",
-        message: "Please provide an example of how your project can be used.",
-        name: "usage"
-      },
-      {
-        type: "list",
-        name: "license",
-        message: "What kind of license would you like to have?",
-        name: "license",
-        choices: ["MIT", "APACHE 2.0", "GPL v3", "BSD 3", "None"]
-      },
-      {
-        type: "input",
-        message: "Including yourself, please list out all contributors",
-        name: "contributer"
-      },
-      {
-        type: "input",
-        message: "What command is used to run a test",
-        name: "tests",
-      }
+    //   {
+    //     type: "input",
+    //     message: "What is URL of your project in GitHub?",
+    //     name: "url"
+    //   },
+    //   {
+    //     type: "input",
+    //     message: "What is the title of your project?",
+    //     name: "title"
+    //   },
+    //   {
+    //     type: "input",
+    //     message: "Please provide a description of your project.",
+    //     name: "description"
+    //   },
+    //   {
+    //     type: "input",
+    //     message: "What packages need to be installed to run your project.",
+    //     name: "installation"
+    //   },
+    //   {
+    //     type: "input",
+    //     message: "What technologies were used to create your project.",
+    //     name: "technology"
+    //   },
+    //   {
+    //     type: "input",
+    //     message: "Please provide an example of how your project can be used.",
+    //     name: "usage"
+    //   },
+    //   {
+    //     type: "list",
+    //     name: "license",
+    //     message: "What kind of license would you like to have?",
+    //     name: "license",
+    //     choices: ["MIT", "APACHE 2.0", "GPL v3", "BSD 3", "None"]
+    //   },
+    //   {
+    //     type: "input",
+    //     message: "Including yourself, please list out all contributors",
+    //     name: "contributer"
+    //   },
+    //   {
+    //     type: "input",
+    //     message: "What command is used to run a test",
+    //     name: "tests",
+    //   }
     ]); // end of return 
   }; // end of function
 
@@ -81,7 +81,7 @@ async function init() {
     try {
         const answers = await askQuestions();
         generateMarkdown(answers);
-        writetoFile("README.md", generateMarkdown(answers));
+        writeToFile("README.md", generateMarkdown(answers));
     
         } catch (err) {
           console.log(err);
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -9,9 +9,9 @@ function generateMarkdown(data) {
 
   ${licenseBadge}
   
-  
   ## Description
     ${data.description}
+
   ## Table of Contents
   - [Installation](#installation)
   - [Usage](#usage)
@@ -19,13 +19,16 @@ function generateMarkdown(data) {
   - [Contributors](#contributors)
   - [Tests](#tests)
   - [Questions](#questions)
+
   ## Installation
   Packages required to run this program are: ${data.installation}
   
   ## Usage
   Examples of how to use this program: ${data.usage}
+
   ## License
-  ${data.license}
+  This application is covered by the ${data.license} license.
+
   ## Tests
   To test, run the following command: ${data.tests}
   
